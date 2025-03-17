dbbc961aa5660f288b51d3bffb68b8cd97d34d96
Mantey <nanakofilarbimantey@gmail.com>
2025-03-17 22:56:20 +0000

    project setup with vite frontend

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Typography } from '@/components/global'
+import { Button } from '@/components/ui'
+import { Link } from 'react-router'
+
+export default function App() {
+	return (
+		<div className='flex flex-col items-center justify-center w-full h-screen gap-8'>
+			<div className='flex flex-col items-center justify-center gap-2'>
+				<Typography as='h1'>nimbus.</Typography>
+				<Typography as='body'>manage your AWS profiles better.</Typography>
+			</div>
+
+			<Link to='/login'>
+				<Button title='get started' />
+			</Link>
+		</div>
+	)
+}
