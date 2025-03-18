9316bb8ce54adfa990b3cda940af6ad4b1f3a375
Mantey <nanakofilarbimantey@gmail.com>
2025-03-18 15:18:15 +0000

    adds copy functionality to aws config

--- src/components/global/EmptyState.tsx
+++ src/components/global/EmptyState.tsx
@@ -1,7 +1,16 @@
-import { IconCloudOff } from '@tabler/icons-react'
+import { IconCloudOff, IconCopy, IconCopyCheck, IconCopyCheckFilled } from '@tabler/icons-react'
 import Typography from './Typography'
+import { useState } from 'react'
 
 export default function EmptyState() {
+	const [hasCopied, setHasCopied] = useState(false)
+
+	const handleCopy = async () => {
+		await navigator.clipboard.writeText('aws configure --profile <profile_name>')
+		setHasCopied(true)
+		setTimeout(() => setHasCopied(false), 2000)
+	}
+
 	return (
 		<div className='flex flex-col border border-dashed border-[#ffffff50] items-center justify-center gap-4 p-4 bg-[#ffffff09] backdrop-blur-md shadow rounded-xl'>
 			<div className='p-4 bg-[#ffffff06] rounded-full'>
@@ -16,9 +25,15 @@ export default function EmptyState() {
 					or <code className='text-[#ffffff90] bg-[#ffffff10] px-1 rounded'>~/.aws/config</code> files. to get started, configure your AWS
 					credentials using the AWS CLI.
 				</Typography>
-				<Typography as='code' className='mt-4 p-2 bg-[#00000050] rounded text-sm'>
-					aws configure
-				</Typography>
+
+				<div className='flex items-center gap-2 mt-4'>
+					<Typography as='code' className='p-2 bg-[#00000050] rounded text-sm'>
+						aws configure --profile &lt;profile_name&gt;
+					</Typography>
+					<button onClick={handleCopy} className='p-2 cursor-pointer hover:bg-[#ffffff10] rounded-full transition-colors duration-200'>
+						{hasCopied ? <IconCopyCheckFilled size={18} className='text-[green]' /> : <IconCopy size={18} className='text-[#ffffff60]' />}
+					</button>
+				</div>
 			</div>
 		</div>
 	)
